872426c79386a0cbabe6510b610b41241f45db18
DanielCubilete <daniel.dominguezcorrea@cieep.com>
2025-04-09 12:14:27 +0200

    IMPORTAR LIBRERIAS

--- GestionalTareas/src/app/components/pages/default/default.component.ts
+++ GestionalTareas/src/app/components/pages/default/default.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-default',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './default.component.html',
   styleUrl: './default.component.css'
 })
